eef37359c2e341eb9f2c40964bebfbd6054ccac0
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2022-05-28 14:17:12 -0700

    Time: 1 ms (100.00%), Space: 73.4 MB (58.74%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i : nums) {
+            sum = Math.max(i, sum + i);
+            max = Math.max(max, sum);
+        }
+        
+        return max;
+    }
+}
