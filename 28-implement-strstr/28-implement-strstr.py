01f214b8a10de7a6a1de6424d92e557fd68eb280
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2022-05-30 20:05:38 -0700

    Time: 33 ms (81.12%), Space: 14 MB (16.66%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,4 +1,18 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack: return haystack.index(needle)
+        if needle == "" or haystack == needle:
+            return 0
+        elif len(needle) > len(haystack):
+            return -1
+        
+        start = 0
+        end = len(needle)
+        
+        while end <= len(haystack):
+            if haystack[start:end] == needle:
+                return start
+            
+            start += 1
+            end += 1
+            
         return -1
