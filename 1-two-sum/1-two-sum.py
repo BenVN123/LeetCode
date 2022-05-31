85b9563d2e4b32722746d4502d2239e147c15981
Ben Nguyen <71541167+BenVN123@users.noreply.github.com>
2022-05-30 18:45:38 -0700

    Time: 50 ms (99.69%), Space: 15.3 MB (24.29%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary = {index:target - num2 for index, num2 in zip(range(len(nums)), nums)}
-
-        for index, required in dictionary.items():
-            try:
-                output = [index, nums.index(required)]
-                if output[0] == output[1]:
-                    assert balls
-            except:
-                pass
-            else:
-                return output
+        seen = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] in seen.keys():
+                return [i, seen[target - nums[i]]]
+            
+            seen[nums[i]] = i
